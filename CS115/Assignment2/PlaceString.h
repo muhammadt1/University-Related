520fcaa3aee317d5b22dc42b0393ec95022ea594
MUHAMMAD TARIQ <muhammad.etariq@gmail.com>
2023-05-09 18:32:59 -0600

    Updated readme

--- CS115/Assignment2/PlaceString.h
+++ CS115/Assignment2/PlaceString.h
@@ -8,4 +8,4 @@ const int INVALID_COORDINATE = -1;
 int stringToInteger(const string &str1);
 int placeStringToRow(const string &str2);
 int placeStringToColumn(const string &str3);
-bool isPlaceStringWellFormed(const string &str4);
+bool isPlaceStringWellFormed(const string &str4); 
